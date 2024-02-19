fde767b07c77fa17dd38b986418b06f0dfd02800
wieberinsma <wiebe.rinsma@han.nl>
2024-02-19 15:26:12 +0100

    Alles

---
+++ src/ProcessingProgram.java
@@ -0,0 +1,9 @@
+import processing.core.PApplet;
+
+public class ProcessingProgram extends PApplet
+{
+    public static void main(String[] args)
+    {
+        PApplet.main("ProcessingProgram");
+    }
+}
